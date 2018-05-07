16cd4c6b15efc0dba8dfc5e27d0cb68b3ff72cab
Jonathan <jaquin92@yahoo.com>
2018-05-07 12:25:47 -0700

    put together a dog list that you can add your favorite dogs too

--- testing/index.js
+++ testing/index.js
@@ -1,5 +1,23 @@
+let dogs = ["pitbull", "poodle"];
+
+
+
 document.getElementById("click-alert").onclick = function () {
     for (i = 1; i <= 10; i++) {
         alert(i)
     }
 }
+
+function getDogs() {
+    for (i = 0; i < dogs.length; i++) {
+        document.getElementById("list").appendChild(document.createElement("li")).innerText = dogs[i]
+    }
+}
+
+document.getElementById("add-dog").onclick = function () {
+    dogs.push(document.getElementById("dog").value)
+    document.getElementById("list").innerHTML = ""
+    getDogs()
+}
+
+getDogs()
